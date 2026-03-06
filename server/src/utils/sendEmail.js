7aedd470f41d612d0f1b9512b1ae00a034f532af
Vansh Khalasi <vanshkhalasi1002@gmail.com>
2026-03-06 10:48:09 +0530

    fix smtp connection

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -8,6 +8,7 @@ const sendEmail = async (to, subject, text) => {
             host:"smtp.gmail.com",
             port:587,
             secure:false,
+            requireTLS:true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
