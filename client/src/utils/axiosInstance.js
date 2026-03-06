a66337432ec67c4f709d0b22329d2de2bb3f22f2
Vansh Khalasi <vanshkhalasi1002@gmail.com>
2026-03-06 09:58:15 +0530

    fix env variable name

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URI,
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 });
 
